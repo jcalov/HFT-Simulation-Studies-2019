10348e08fc0f05d089844e255326b062986367ea
Nochos con queso <nachos@con-queso.de>
2019-02-20 09:21:22 +0100

    Update of package MLANP

--- provided_by_chiodi/stuttgart_for_windows.R
+++ provided_by_chiodi/stuttgart_for_windows.R
@@ -1,5 +1,5 @@
 ## it should work in R under windows
 install.packages(c("rpanel","ks","sm","misc3d", "shiny", "magrittr", "crosstalk", "manipulateWidget"))
 install.packages("rgl_0.99.16.zip",repos=NULL, type="source")
-install.packages("MLANP_1.1.tar.gz",repos=NULL,type="source")
+install.packages("MLANP_1.2.tar.gz",repos=NULL,type="source")
 library(MLANP)
