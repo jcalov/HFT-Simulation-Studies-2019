ca0cca818dd9dc6945407272ed967534c274aa8d
Nochos con queso <nachos@con-queso.de>
2019-02-22 09:39:14 +0100

    Last Assignments provided by Marcello Chiodi

---
+++ provided_by_chiodi/assignment2.R
@@ -0,0 +1,14 @@
+sample1=read.table("sample1.txt",head=TRUE)
+
+
+## assignment:
+#  read the file sample1 200 obs  6 variables
+#
+## draw the density estimation of the variables 
+## weight and height
+## make the same thing separately for the two groups defined by status
+## try to draw the two densities for each var on the same graph
+##
+##
+##second: draw the non parametric regression lines using the 
+# height as a function of week for the whole sample and for the two groups defined by status
